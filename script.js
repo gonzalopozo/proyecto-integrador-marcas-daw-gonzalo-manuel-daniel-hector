c429d487a1688d1514e9dab74c95d178c460eef6
Héctor Augusto <changoh3ctor23@gmail.com>
2024-05-18 12:33:50 +0200

    Implementacion de Comentarios e Indicaciones de funciones

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function toggleMenu(){
+
+function toggleMenu() {
+    // Buscar el elemento HTML que tiene la clase 'toggle' y guardarlo en la variable menuToggle
     var menuToggle = document.querySelector('.toggle');
+    // Buscar el elemento HTML que tiene la clase 'navegacion' y guardarlo en la variable navegacion
     var navegacion = document.querySelector('.navegacion');
+    // Alternar la clase 'active' en el elemento menuToggle
+    // Esto cambiará la apariencia visual del botón que activa el menú de navegación
     menuToggle.classList.toggle('active');
+    // Alternar la clase 'active' en el elemento navegacion
+    // Esto mostrará u ocultará el menú de navegación en la página web
     navegacion.classList.toggle('active');
 }
 
+
 function subir() {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); // Desplazar la ventana a la posición (0, 0)
 }
 
 document.addEventListener("DOMContentLoaded", function() {
