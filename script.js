2bd3a82435e4e497d5d7f387c403bb73ecc768e6
Manuel Gomez <mgn14@hotmail.es>
2024-05-20 19:07:56 +0200

    Versión Final 3.0

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-
 function toggleMenu() {
-    // Buscar el elemento HTML que tiene la clase 'toggle' y guardarlo en la variable menuToggle
+    // Buscar el elemento HTML con la clase 'toggle' y guardarlo en la variable menuToggle
     var menuToggle = document.querySelector('.toggle');
-    // Buscar el elemento HTML que tiene la clase 'navegacion' y guardarlo en la variable navegacion
+    
+    // Buscar el elemento HTML con la clase 'navegacion' y guardarlo en la variable navegacion
     var navegacion = document.querySelector('.navegacion');
+    
     // Alternar la clase 'active' en el elemento menuToggle
     // Esto cambiará la apariencia visual del botón que activa el menú de navegación
     menuToggle.classList.toggle('active');
+    
     // Alternar la clase 'active' en el elemento navegacion
     // Esto mostrará u ocultará el menú de navegación en la página web
     navegacion.classList.toggle('active');
 }
 
-
 function subir() {
-    window.scrollTo(0, 0); // Desplazar la ventana a la posición (0, 0)
+    // Desplazar la ventana a la posición (0, 0)
+    window.scrollTo(0, 0);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     // Se ejecuta cuando la página se carga por completo
 
+    // Seleccionar todos los elementos con la clase 'boton-personajes' y guardarlos en la variable botones
     var botones = document.querySelectorAll('.boton-personajes');
 
     // Iterar sobre cada botón
     botones.forEach(function(boton) {
-        // Agregar evento click al botón actual
+        // Agregar un evento de click al botón actual
         boton.addEventListener("click", function(event) {
-            // Prevenir el comportamiento predeterminado del botón (llevar al principio de la página)
+            // Prevenir el comportamiento predeterminado del botón (por ejemplo, el envío de un formulario)
             event.preventDefault();
 
-            // Obtener el contenedor padre del botón actual
+            // Obtener el contenedor padre del botón actual con la clase 'ficha'
             var contenedorPadre = boton.closest('.ficha');
 
-            // Encontrar el elemento de texto de personajes dentro del contenedor padre
+            // Encontrar el elemento con la clase 'texto-personajes' dentro del contenedor padre
             var textoPersonajes = contenedorPadre.querySelector('.texto-personajes');
 
-            // Alternar clase al texto de personajes asociado al botón actual
+            // Si el elemento de texto de personajes existe, alternar la clase 'active-personajes'
             if (textoPersonajes) {
                 textoPersonajes.classList.toggle('active-personajes');
             }
